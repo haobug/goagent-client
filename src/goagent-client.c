ac1983bb471563c694949b0b3d51d9ef72109488
Changming Sun <me@sunchangming.com>
2013-02-18 04:08:58 -0800

    add libevent

--- src/goagent-client.c
+++ src/goagent-client.c
@@ -17,10 +17,26 @@
 #include <stdlib.h>
 #include <signal.h>
 
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
+#include <event2/event.h>
+
 #include "goagent-client.h"
 
 int main(int argc, char **argv)
 {
+	struct event_base *base;
+    struct evconnlistener *listener;
+    struct event *signal_event;
+
+    struct sockaddr_in sin;
+#ifdef WIN32
+    WSADATA wsa_data;
+    WSAStartup(0x0201, &wsa_data);
+#endif
+
 	configuration config;
     printf("=================GoAgent native client================\r\n");
     printf("Version 0.1.0\r\n\r\n");
@@ -30,5 +46,8 @@ int main(int argc, char **argv)
         return 1;
     }
     printf("test\r\nlisten:%s:%d\r\ngae id:%s hosts:%s\r\n",config.listen_ip,config.listen_port,config.gae_appid,config.current_profile->hosts);
+
+
+    base = event_base_new();  
     return 0;
 }
