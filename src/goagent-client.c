b23aebb609bf86069e2092e58f47ed50476b7f38
snnn <me@sunchangming.com>
2013-02-19 14:59:48 +0800

    启动libevent loop

--- src/goagent-client.c
+++ src/goagent-client.c
@@ -25,13 +25,144 @@
 
 #include "goagent-client.h"
 
+
+static void read_cb(struct bufferevent *bev, void *ctx){
+    struct evbuffer *input;
+    char *request_line;
+    size_t len;
+    
+    input = bufferevent_get_input(bev);
+    request_line = evbuffer_readln(input, &len, EVBUFFER_EOL_CRLF);
+    if (!request_line) {
+        /* The first line has not arrived yet. */
+        return;
+    } else {
+        fprintf(stderr,"received line %s\n",request_line);
+        free(request_line);
+    }
+}
+
+static void
+event_cb(struct bufferevent *bev, short events, void *ctx)
+{
+    if (events & BEV_EVENT_ERROR)
+        perror("Error from bufferevent");
+    if (events & (BEV_EVENT_EOF | BEV_EVENT_ERROR)) {
+        bufferevent_free(bev);
+    }
+}
+
+static void my_acceptcb(struct evconnlistener *listener,
+                 evutil_socket_t sock, struct sockaddr *addr, int addrlen, void *ptr){
+    struct event_base *base;
+    char host[NI_MAXHOST];
+    int rv;
+    
+    rv = getnameinfo(addr, addrlen, host, sizeof(host), 0, 0, NI_NUMERICHOST);
+    if(rv){
+        fprintf(stderr,"getnameinfo() failed: %s\n",gai_strerror(rv));
+        return ;
+    }
+    fprintf(stderr,"got connection from %s\n",host);
+    base = evconnlistener_get_base(listener);
+    struct bufferevent *bev = bufferevent_socket_new(base, sock, BEV_OPT_CLOSE_ON_FREE);
+    
+    bufferevent_setcb(bev, read_cb, NULL, event_cb, NULL);
+    
+    bufferevent_enable(bev, EV_READ|EV_WRITE);
+}
+
+static void
+accept_error_cb(struct evconnlistener *listener, void *ctx)
+{
+    struct event_base *base = evconnlistener_get_base(listener);
+    int err = EVUTIL_SOCKET_ERROR();
+    fprintf(stderr, "Got an error %d (%s) on the listener. "
+            "Shutting down.\n", err, evutil_socket_error_to_string(err));
+    
+    event_base_loopexit(base, NULL);
+}
+
+/**
+ * 创建evconnlistener
+ */
+static struct evconnlistener* create_evlistener(struct event_base *base,int family,configuration* config)
+{
+    // TODO Listen both IPv4 and IPv6 ?
+    struct addrinfo hints;
+    int fd = -1;
+    int r;
+    char service[10];
+    struct addrinfo *res, *rp;
+
+    int val = 1;
+    
+    snprintf(service, sizeof(service), "%u", config->listen_port);
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = family;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    
+    r = getaddrinfo(config->listen_ip, service, &hints, &res);
+    if(r != 0) {
+        /* LOG(INFO) << "Unable to get IPv" << (family == AF_INET ? "4" : "6")
+        << " address for " << get_config()->host << ": "
+        << gai_strerror(r);*/
+        return NULL;
+    }
+    
+    //把getaddrinfo返回的addrinfo挨个尝试一遍
+    for(rp = res; rp; rp = rp->ai_next) {
+        fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if(fd == -1) {
+            continue;
+        }       
+        if(setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val,
+                      (socklen_t)(sizeof(val))) == -1) {
+            close(fd);
+            continue;
+        }
+        evutil_make_socket_nonblocking(fd);
+        
+        if(bind(fd, rp->ai_addr, rp->ai_addrlen) == 0) {
+            break;
+        }
+        close(fd);
+    }
+    if(rp) {
+        char host[NI_MAXHOST];
+        r = getnameinfo(rp->ai_addr, rp->ai_addrlen, host, sizeof(host),
+                        0, 0, NI_NUMERICHOST);
+        if(r == 0) {
+            fprintf(stdout,"Listening on %s port %d \n",config->listen_ip,config->listen_port);
+        } else {
+            fprintf(stdout,"err,%s",gai_strerror(r));
+            exit(-1);
+        }
+    }
+    freeaddrinfo(res);
+    if(rp == 0) {
+        return NULL;
+    }
+    
+    struct evconnlistener *evlistener = evconnlistener_new
+    (base,
+     my_acceptcb,//listener callback
+     NULL, //TODO: pass handler
+     LEV_OPT_REUSEABLE | LEV_OPT_CLOSE_ON_FREE,
+     10, //TODO: setting it in config file. this is the backlog param in listen(2) system call
+     fd);
+    evconnlistener_set_error_cb(evlistener, accept_error_cb);
+    return evlistener;
+}
+
 int main(int argc, char **argv)
 {
 	struct event_base *base;
     struct evconnlistener *listener;
-    struct event *signal_event;
+    //struct event *signal_event;
 
-    struct sockaddr_in sin;
+    //struct sockaddr_in sin;
     pid_t  pid, sid;
 #ifdef WIN32
     WSADATA wsa_data;
@@ -67,6 +198,14 @@ int main(int argc, char **argv)
         close(STDERR_FILENO);
     }
 
-    base = event_base_new();  
+    base = event_base_new();
+    listener=create_evlistener(base,AF_INET,&config);
+    if(listener){
+        fprintf(stdout,"create listener successed, enter loop\n");
+    }
+    event_base_loop(base, 0);
+    if(listener) {
+        evconnlistener_free(listener);
+    }
     return 0;
 }
